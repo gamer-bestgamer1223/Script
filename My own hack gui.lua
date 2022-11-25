76612711396b1493488d3f754130a9df9569ee1b
gamer-bestgamer1223 <119070804+gamer-bestgamer1223@users.noreply.github.com>
2022-11-25 12:59:40 -0500

    Add files via upload

--- My own hack gui.lua
+++ My own hack gui.lua
@@ -1,5 +1,5 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("My first ever GUI", "OceanTheme")
+local Window = Library.CreateLib("My first ever GUI", "Ocean")
 
 -- MAIN
 local Main = Window:NewTab("Main")
